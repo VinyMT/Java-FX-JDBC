bef5596417108c52d8c44f36549bd721b047070a
VinyMT <viniciusmatiolatramontin@gmail.com>
2021-12-22 12:59:30 -0300

    Implemented a method to save new departments in the database

--- src/view/DepartmentFormController.java
+++ src/view/DepartmentFormController.java
@@ -3,16 +3,23 @@ package view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
+import view.util.Alerts;
 import view.util.Constraints;
+import view.util.Utils;
 
 public class DepartmentFormController implements Initializable {
 	
+	private DepartmentService service;
 	private Department dep;
 	
     @FXML
@@ -31,11 +38,32 @@ public class DepartmentFormController implements Initializable {
 	private Label lblError;
 	
     @FXML
-    public void onBtnSaveAction() {
-    	System.out.println("onBtnSaveAction");
+    public void onBtnSaveAction(ActionEvent ev) {
+    	if(dep == null) {
+    		throw new IllegalStateException("Department was null");
+    	}
+       	if(service == null) {
+    		throw new IllegalStateException("Service was null");
+    	}
+       	try {
+       		dep = getFormData();
+       		service.saveOrUpdate(dep);
+       		Utils.currentStage(ev).close();
+       	} catch(DbException e) {
+       		Alerts.showAlert("Error!", null, e.getMessage(), AlertType.ERROR);
+       	}
     }
     
-    @FXML
+    private Department getFormData() {
+    	Department department = new Department();
+    	
+    	department.setId(Utils.tryParseToInt(tfId.getText()));
+    	department.setName(tfName.getText());
+    	
+    	return department;
+	}
+
+	@FXML
     public void onBtnCancelAction() {
     	System.out.println("onBtnCancelAction");
     }
@@ -51,13 +79,17 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldMaxLength(tfName, 30);
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
 	public void setDepartment(Department dep) {
 		this.dep = dep;
 	}
 	
 	public void updateFormData() {
 		if(dep == null) {
-			throw new IllegalStateException("Dep was null!");
+			throw new IllegalStateException("Department was null!");
 		}
 		tfId.setText(String.valueOf(dep.getId()));
 		tfName.setText(dep.getName());
--- src/view/util/Utils.java
+++ src/view/util/Utils.java
@@ -8,4 +8,12 @@ public class Utils {
 	public static Stage currentStage(ActionEvent ev) {
 		return (Stage) ((Node) ev.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch(NumberFormatException e) {
+			return null;
+		}
+	}
 }
